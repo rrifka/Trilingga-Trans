abf3eb47a306fa66de7ce3662dda0094d2b632a1
rrifka <rifkatasyaramadhani18@gmail.com>
2024-09-03 10:07:00 +0700

    update

--- main.js
+++ main.js
@@ -23,22 +23,22 @@ const swiper = new Swiper(".swiper", {
     loop: true,
 });
 
-//responsif header
-const optionMenu = document.querySelector(".select-menu"),
-       selectBtn = optionMenu.querySelector(".select-btn"),
-       options = optionMenu.querySelectorAll(".option"),
-       sBtn_text = optionMenu.querySelector(".sBtn-text");
+// //responsif header
+// const optionMenu = document.querySelector(".select-menu"),
+//        selectBtn = optionMenu.querySelector(".select-btn"),
+//        options = optionMenu.querySelectorAll(".option"),
+//        sBtn_text = optionMenu.querySelector(".sBtn-text");
 
-selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+// selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
 
-options.forEach(option =>{
-    option.addEventListener("click", ()=>{
-        let selectedOption = option.querySelector(".option-text").innerText;
-        sBtn_text.innerText = selectedOption;
+// options.forEach(option =>{
+//     option.addEventListener("click", ()=>{
+//         let selectedOption = option.querySelector(".option-text").innerText;
+//         sBtn_text.innerText = selectedOption;
 
-        optionMenu.classList.remove("active");
-    });
-});
+//         optionMenu.classList.remove("active");
+//     });
+// });
 
 // // Menambahkan event listener pada dropdown toggle
 // const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
